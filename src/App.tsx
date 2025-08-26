6299d91f986c73a1cafab6a31a2057f6be22aa8c
fratjo <francois.tjoens.pro@gmail.com>
2025-08-26 20:58:29 +0200

    Config shadcnUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-function App() {
+import { Button } from '@/components/ui/button'
+
+const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello Vite + React!</h1>
+      <Button className='bg-blue-500 text-white py-2 px-4 rounded'>Click me</Button>
     </>
   );
 }
